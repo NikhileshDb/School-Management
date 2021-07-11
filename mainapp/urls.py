5b3e9e633ae1b50dcff993db7a519539d073b6df
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-07-12 00:26:29 +0530

    yeahthis

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,9 +6,9 @@ from .TeacherViews import *
 from .PrincipalApiViews import *
 urlpatterns =[
     path('', index, name="index" ),
-    path('add_student/', add_students, name="add_student" ),
-    path('student_list/', StudentList, name="student_list"),
-    path('add_teacher/', add_teachers, name="add_teacher" ),
-    path('teacher_list/', TeacherList, name="teacher_list" ),
+    path('api/add_student/', add_students, name="add_student" ),
+    path('api/student_list/', StudentList, name="student_list"),
+    path('api/add_teacher/', add_teachers, name="add_teacher" ),
+    path('api/teacher_list/', TeacherList, name="teacher_list" ),
 ]
 
--- sms/settings.py
+++ sms/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z(&fyt+5i*%u1sj=&o3(+i4-k4*=a(jgekpdwi28pxlh6-a6^t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['smsgaria.herokuapp.com', '127.0.0.1:8000']
 
 
 # Application definition
