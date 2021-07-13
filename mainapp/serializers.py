e05031fca7dd79d8f0e6f7a044cf83e27ed6ab12
Louchchee <louchchee@gmail.com>
2021-07-13 18:26:27 +0530

    Data added and uncommented

--- mainapp/serializers.py
+++ mainapp/serializers.py
@@ -22,10 +22,25 @@ class StudentsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Students
         fields = [
-            'customuser', 
+            'customuser',
+            'middleName',
             'section', 
             'dob',
             'gender',
+            'fatherName',
+            'motherName',
+            'current_address',
+            'parmanent_address',
+            'religion',
+            'phoneNumber',
+            'nationality',
+            'created_at',
+            'updated_at',
+            'profile_pic',
+            'blood_group',
+            'classRoom',
+            'objects',
+
         ]
         
     def create(self, validated_data):
@@ -35,8 +50,23 @@ class StudentsSerializer(serializers.ModelSerializer):
             validated_data=customuser_data)
         student, created = Students.objects.update_or_create(
             customuser = customuser,
+            middleName = validated_data.pop('middleName'),
             section = validated_data.pop('section'),
             dob = validated_data.pop('dob'),
-            gender = validated_data.pop('gender')
+            gender = validated_data.pop('gender'),
+            fatherName = validated_data.pop('fatherName'),
+            motherName = valdated_data.pop('motherName'),
+            current_address = validated_data.pop('current_address'),
+            parmanent_address = validated_data.pop('permanent_address'),
+            religion = validated_data.pop('religion'),
+            phoneNumber = validated_data.pop('phoneNumber'),
+            nationality = validated_data.pop('nationality'),
+            created_at = validated_data.pop('created_at'),
+            updated_at = validated_data.pop('updated_at'),
+            profile_pic = validated_data('profile_pic'),
+            blood_group = validated_data('blood_group'),
+            classRoom = validated_data('classRoom'),
+            objects = validated_data('objects'),
+            
         )
         return student
