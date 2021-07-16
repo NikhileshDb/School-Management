a47973a96a974e42bb6b4ff60ec45f4c41f0829b
Nikhilesh <nikhilesh.debbarma062@gmail.com>
2021-07-16 20:39:04 +0530

    Kalka

--- mainapp/models.py
+++ mainapp/models.py
@@ -65,3 +65,4 @@ class Students(models.Model):
     
 class ProfileImage(models.Model):
     image = models.ImageField(_("Image"),upload_to="student_profile/", default="media/default.png" , blank=True, null=True )
+    desc =  models.CharField(max_length=20, blank=True, null=True)
